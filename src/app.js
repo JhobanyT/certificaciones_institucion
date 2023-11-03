0708f24e60ad24db49d0e694ea5c4ae377f50292
JhobanyT <jhobanyticona@gmail.com>
2023-11-02 21:01:08 -0500

    completo

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const insRouter = require('./routes/insRouter.js');
+const documentoRouter = require('./routes/documentoRouter.js');
+
+const app = express();
+
+// Configurar encabezados de seguridad
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
+    next();
+});
+
+    // Manejar errores de forma segura
+    app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Error interno del servidor' });
+});
+
+
+app.use(express.json());
+
+app.use('/api', documentoRouter);
+app.use('/api', insRouter);
+
+module.exports = app;
